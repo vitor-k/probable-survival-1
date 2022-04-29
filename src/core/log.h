2f0644620fa81cf91a0d552b6d56476145e3fc9f
Vitor Kiguchi <vitor-kiguchi@hotmail.com>
2022-04-29 13:28:48 +0200

    it's _DEBUG not _debug

--- src/core/log.h
+++ src/core/log.h
@@ -17,7 +17,7 @@ extern fmt::ostream out;
 template<typename... T>
 inline void log(LogLevel level, const char* s, T&&... args) {
     if(level == LogLevel::Debug) {
-#ifdef _debug
+#ifdef _DEBUG
         out.print(s, args...);
         fmt::print(s, args...);
 #endif
@@ -30,7 +30,7 @@ inline void log(LogLevel level, const char* s, T&&... args) {
 
 #define LOG(...) log(::LogLevel::Normal, __VA_ARGS__)
 
-#ifdef _debug
+#ifdef _DEBUG
 #define LOG_DEBUG(...) log(::LogLevel::Debug, __VA_ARGS__)
 #else
 #define LOG_DEBUG(...) (void(0))
